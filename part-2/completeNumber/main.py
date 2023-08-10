2ab7de9d4167d87d401818df54552ee27deac360
Arian <arian.koochak@gmail.com>
2023-08-10 09:46:10 +0330

    Complete Number(optimize)

--- part-2/completeNumber/main.py
+++ part-2/completeNumber/main.py
@@ -1,8 +1,8 @@
-n = int(input('enter your number :'))
+n = int(input('enter your number : '))
 
 sum = 0
 
-for i in range(1,n):
+for i in range(1,n//2+1):
     if n // i == n / i:
         sum += i
 
