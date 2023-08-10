82ec0142abd306561ac65d1b3647e58fc1268a9f
Arian <arian.koochak@gmail.com>
2023-08-10 09:48:10 +0330

    prime Number(optimize)

--- part-2/primeNumber/main.py
+++ part-2/primeNumber/main.py
@@ -1,11 +1,11 @@
 inp = int(input('enter integer number: '))
 isPrime = True
 
-if inp <= 0:
+if inp <= 0 or inp%2 == 0:
     isPrime = False
 else:
     counter = int(inp/2)+1
-    for i in range(2,counter):
+    for i in range(3,counter,2):
         if inp // i == inp / i:
             isPrime = False
             break
