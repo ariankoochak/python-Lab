51b5259ceaa58033e2a2ede0ad4f71b296d2a0ec
Arian <arian.koochak@protonmail.com~>
2023-08-11 17:40:34 +0330

    sentence works(optimize)

--- part-3/sentenceWorks/main.py
+++ part-3/sentenceWorks/main.py
@@ -3,44 +3,29 @@ upperCaseInp = ''
 lowerCaseInp = ''
 toggleCaseInp = ''
 wordCaseInp = ''
-# all capital
+
 for i in range(0,len(inp)):
     charCode = ord(inp[i])
     if charCode <= 122 and charCode >= 97:
+        if i == 0 or ord(inp[i-1]) == 32:
+            wordCaseInp += chr(charCode - 32)
+        else:
+            wordCaseInp += inp[i]
         upperCaseInp += chr(charCode - 32)
+        toggleCaseInp += chr(charCode - 32)
+        lowerCaseInp += inp[i]
+    elif charCode <= 90 and charCode >= 65:
+        lowerCaseInp += chr(charCode + 32)
+        toggleCaseInp += chr(charCode + 32)
+        upperCaseInp += inp[i]
+        wordCaseInp += inp[i]
     else:
         upperCaseInp += inp[i]
-
-print(f'\nall upperCase:\n{upperCaseInp}')
-
-# all lower
-for i in range(0,len(inp)):
-    charCode = ord(inp[i])
-    if charCode <= 90 and charCode >= 65:
-        lowerCaseInp += chr(charCode + 32)
-    else:
         lowerCaseInp += inp[i]
-
-print(f'\nall lowerCase:\n{lowerCaseInp}')
-
-# toggle
-for i in range(0,len(inp)):
-    charCode = ord(inp[i])
-    if charCode <= 90 and charCode >= 65:
-        toggleCaseInp += chr(charCode + 32)
-    elif charCode <= 122 and charCode >= 97:
-        toggleCaseInp += chr(charCode - 32)
-    else:
         toggleCaseInp += inp[i]
-
-print(f'\ntoggleCase:\n{toggleCaseInp}')
-
-# word case
-for i in range(0,len(inp)):
-    charCode = ord(inp[i])
-    if charCode <= 122 and charCode >= 97 and (i == 0 or ord(inp[i-1]) == 32):
-        wordCaseInp += chr(charCode - 32)
-    else:
         wordCaseInp += inp[i]
 
+print(f'\nall upperCase:\n{upperCaseInp}')
+print(f'\nall lowerCase:\n{lowerCaseInp}')
+print(f'\ntoggleCase:\n{toggleCaseInp}')
 print(f'\nwordcase:\n{wordCaseInp}')
