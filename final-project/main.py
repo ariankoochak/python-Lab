137048d0d9253dba8f70b75efd15d418f754b086
Arian <arian.koochak@gmail.com>
2023-09-30 17:00:18 +0330

    clean factors

--- final-project/main.py
+++ final-project/main.py
@@ -4,8 +4,9 @@ import os
 # commodityPath = "c:\\Users\\Arian\\Desktop\\Code\python-Lab\\final-project\\data\\commodity.csv" for windows
 commodityPath = "/Users/arian.koochakgmail.com/Desktop/Code/python-Lab/final-project/data/commodity.csv"
 costumerPath = '/Users/arian.koochakgmail.com/Desktop/Code/python-Lab/final-project/data/costumers.csv'
+factorPath = '/Users/arian.koochakgmail.com/Desktop/Code/python-Lab/final-project/data/factor.csv'
 
 commodities = cleanFile(readFile(commodityPath))
 costumers = cleanFile(readFile(costumerPath))
-
-showDatas(commodities)
+factors = cleanFactorFile(readFile(factorPath),commodities)
+showDatas(factors)
--- final-project/modules.py
+++ final-project/modules.py
@@ -26,3 +26,36 @@ def showDatas(inp):
         counter += 1
         for i in inp[key]:
             print("%15s"%inp[key][i],end="")
+def handleOrderInFactor(inp):
+    inp = inp.split(';')
+    for i in range(len(inp)):
+        inp[i] = inp[i].split(':')
+    return inp
+
+def handlePaymentSitInFactor(inp):
+    return (inp.upper() == 'TRUE')
+
+def calculateSumInFactor(orders,inp,commoditiesLits):
+    exp = 0
+    for item in orders:
+        exp += int(commoditiesLits[item[0]]['price']) * int(item[1])
+    return exp
+
+def cleanFactorFile(inp,commoditiesLits):
+    exp = {}
+    inp[0] = inp[0][:-1].split(',')
+    for i in range(1,len(inp)):
+        inp[i] = inp[i][:-1].split(',')
+        preDict = {}
+        for j in range(1,len(inp[i])):
+            if inp[0][j] == 'payment_sit':
+                preDict[inp[0][j]] = handlePaymentSitInFactor(inp[i][j])
+            elif inp[0][j] == 'orders':
+                preDict[inp[0][j]] = handleOrderInFactor(inp[i][j])
+            elif inp[0][j] == 'sum':
+                preDict[inp[0][j]] = calculateSumInFactor(preDict['orders'],inp[i][j],commoditiesLits)
+            else:
+                preDict[inp[0][j]] = inp[i][j]
+        exp[inp[i][0]] = preDict
+        # print(exp)
+    return exp
