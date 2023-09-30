4b1fb0d5a0bdfa26a9b5d3118c47ab1617d4e43f
Arian <arian.koochak@gmail.com>
2023-09-30 16:18:39 +0330

    add costumers file

--- final-project/main.py
+++ final-project/main.py
@@ -1,8 +1,11 @@
 from modules import *
 import os
 
-commodityPath = "c:\\Users\\Arian\\Desktop\\Code\python-Lab\\final-project\\data\\commodity.csv"
+# commodityPath = "c:\\Users\\Arian\\Desktop\\Code\python-Lab\\final-project\\data\\commodity.csv" for windows
+commodityPath = "/Users/arian.koochakgmail.com/Desktop/Code/python-Lab/final-project/data/commodity.csv"
+costumerPath = '/Users/arian.koochakgmail.com/Desktop/Code/python-Lab/final-project/data/costumers.csv'
 
 commodities = cleanFile(readFile(commodityPath))
+costumers = cleanFile(readFile(costumerPath))
 
 showDatas(commodities)
